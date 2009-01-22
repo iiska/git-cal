207cf8b8639e6de399a7031cbeded39a6dff0d23
Juhamatti Niemelä <iiska@iki.fi>
2009-01-22 20:45:15 +0200

    Simple log output handling and html rendering.

--- git-cal.rb
+++ git-cal.rb
@@ -1,1 +1,14 @@
 #! /usr/bin/env ruby
+
+require 'erb'
+
+log = `git-log --pretty=format:'%an|%at|%s'`.split("\n").map do |line|
+  items = line.split('|')
+  {:author => items[0], :timestamp => items[1], :message => items[2]}
+end
+
+template = File.open("template.html.erb"){|f| f.read}
+
+rhtml = ERB.new(template)
+
+puts rhtml.result
